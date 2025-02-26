93db3b39d4462cdd0f13c1b545e965f61ae3286a
Ryan Perez <perezryan26@gmail.com>
2025-02-25 22:49:13 -0600

    Add shadcn ui library

--- src/landingPage/components/LandingPageContent.tsx
+++ src/landingPage/components/LandingPageContent.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function LandingPageContent() {
     return (
-        <div>
+        <div className="">
             <h1>this is where the content goes</h1>
+            <Button> Testing shadcn button</Button>
         </div>
     );
 }
